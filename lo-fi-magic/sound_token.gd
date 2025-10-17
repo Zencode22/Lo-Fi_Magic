0894cb8cb98604e15883bbd8708f5089950778be
That 1 Guy <n.vihn1@gmail.com>
2025-10-17 10:38:45 -0500

    Token Update

--- lo-fi-magic/sound_token.gd
+++ lo-fi-magic/sound_token.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
 @export var speed : float = 5
-
+#@export var tokenColor : Color = Color(251,63, 255, 255)
+#@onready var mesh = $Sound_Token/Circle
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#mesh.get_material_override().albedo_color = tokenColor
 	pass # Replace with function body.
 
 
