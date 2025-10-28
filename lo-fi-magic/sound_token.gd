b40202fc4686f77cd620d7644e5a1e860a4dc9b8
Connor Lonergan <connorlonergan@proton.me>
2025-10-27 21:33:14 -0500

    adjusted sound token file

--- lo-fi-magic/sound_token.gd
+++ lo-fi-magic/sound_token.gd
@@ -1,15 +1,11 @@
 extends Node3D
 
 @export var speed : float = 5
-#@export var tokenColor : Color = Color(251,63, 255, 255)
-#@onready var mesh = $Sound_Token/Circle
-# Called when the node enters the scene tree for the first time.
+@onready var mesh = $Sound_Token/Circle
+
 func _ready() -> void:
-	#mesh.get_material_override().albedo_color = tokenColor
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate_object_local(Vector3.UP, (speed * delta)) 
 	pass
